0eb35d40f71aa873e943b1b3bbaa17a78865a176
marimontefusco <mari.montefusco@gmail.com>
2022-10-07 17:23:22 -0300

    Chapter 1 -> setting up react  environments

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './styles/global.scss';
+// import React from 'react'
+// import { App } from './App'
+
+export function App() {
+    return <h1>Hello World</h1>
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { render } from 'react-dom';
+import { App } from './App';
+
+render(<App />, document.getElementById('root'))
+
+//render(param1, param2)
+//param1 -> o que eu quero renderizar, o q quqero exibir em tela?
+//param2 -> dentro de qual elemento quero renderizar essa informação? lá na div id='root' do meu documento html
